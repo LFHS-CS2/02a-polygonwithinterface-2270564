ef174a8d02eed00d112f6c27803b1324c72a46ad
2270564 <70032335+2270564@users.noreply.github.com>
2020-09-11 13:55:54 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,13 @@ class Main {
   }
 public static Shape2D[] getShapes()
 {
-  //Do I create my own?
+  Circle circ = new Circle(4.0);
+  Rectangle rect = new Rectangle(4, 2);
+  Triangle tri = new Triangle(3, 3, 3);
+  Shape2D[] shapey= new Shape2D[3];
+  shapey[0] = circ;
+  shapey[1] = tri;
+  shapey[2] = rect;
+  return shapey;
 }
 }
