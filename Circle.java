9153807ae81c83b402ec3c05ebe5f6be887353e8
2270564 <70032335+2270564@users.noreply.github.com>
2020-09-11 13:56:30 -0500

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,8 +1,20 @@
-public abstract class Circle implements Shape2D
+public class Circle implements Shape2D
  {
   public double radius;
+  public Circle(double rad)
+  {
+    radius = rad;
+  }
   public void setRadii(double rad)
   {
     radius = rad;
+  }
+  public double getArea()
+  {
+    return (radius*radius)*PI;
+  }
+  public double getPerimeter()
+  {
+    return 2*radius*PI;
   }
 }
